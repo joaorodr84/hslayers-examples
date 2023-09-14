b970722f208dbea0d2d40eac242d8e83ad709fba
João Rodrigues <joao.rodrigues@ubiquity.pt>
2023-09-14 15:06:00 +0100

    routing
    # Conflicts:
    #	package-lock.json
    #	package.json

--- full/src/app.module.ts
+++ full/src/app.module.ts
@@ -1,12 +1,28 @@
-import {AppComponent} from './app.component';
-import {BrowserModule} from '@angular/platform-browser';
-import {HslayersModule} from 'hslayers-ng';
-import {NgModule} from '@angular/core';
+import { AppComponent } from "./app.component";
+import { BrowserModule } from "@angular/platform-browser";
+import { HslayersModule } from "hslayers-ng";
+import { NgModule } from "@angular/core";
+import { MapComponent } from "./app/map/map.component";
+import { HomeComponent } from "./app/home/home.component";
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'map', component: MapComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home' },
+];
 
 @NgModule({
-  imports: [BrowserModule, HslayersModule],
-  declarations: [AppComponent],
-  exports: [],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    HslayersModule
+  ],
+  declarations: [AppComponent, MapComponent, HomeComponent],
+  exports: [
+    RouterModule
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
